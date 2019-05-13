d0a06aa9ea1d87ad93ee3aab1df8c35fd4b9f978
Shubham Garg <shubham1389@gmail.com>
2019-05-13 19:11:49 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='test-egg-package',
+    version='0.1.0',
+    description='create egg package',
+    long_description='Just do a POC for creating an egg',
+    author='Shubham Garg',
+    author_email='shubham1389@gmail.com',
+    url='git@github.com:shubham1389/test_egg.git',
+    packages=['test_egg', ],
+)
